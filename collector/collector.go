dcac2446ba1b131b7e3c1818b2abeb062203c4b8
Pavel Borzenkov <pavel@borzenkov.net>
2021-07-26 11:17:18 +0300

    collector: remove 'portOpen' metric
    It's very unreliable.

--- collector/collector.go
+++ collector/collector.go
@@ -17,14 +17,13 @@ type TransmissionCollector struct {
 	client *transmission.Client
 	logger log.Logger
 
-	portOpenDesc   *prometheus.Desc
 	turtleModeDesc *prometheus.Desc
 
-	activeTorrents *prometheus.Desc
-	pausedTorrents *prometheus.Desc
+	activeTorrentsDesc *prometheus.Desc
+	pausedTorrentsDesc *prometheus.Desc
 
-	downloadedBytesTotal *prometheus.Desc
-	uploadedBytesTotal   *prometheus.Desc
+	downloadedBytesTotalDesc *prometheus.Desc
+	uploadedBytesTotalDesc   *prometheus.Desc
 }
 
 func NewTransmissionCollector(client *transmission.Client, logger log.Logger) (*TransmissionCollector, error) {
@@ -32,34 +31,29 @@ func NewTransmissionCollector(client *transmission.Client, logger log.Logger) (*
 		client: client,
 		logger: logger,
 
-		portOpenDesc: prometheus.NewDesc(
-			prometheus.BuildFQName(namespace, "", "is_port_open"),
-			"Indicates whether or not the peer port is accessible from the Internet.",
-			nil, nil,
-		),
 		turtleModeDesc: prometheus.NewDesc(
 			prometheus.BuildFQName(namespace, "", "is_turtle_mode_active"),
 			"Indicates whether or not turtle mode is active.",
 			nil, nil,
 		),
 
-		activeTorrents: prometheus.NewDesc(
+		activeTorrentsDesc: prometheus.NewDesc(
 			prometheus.BuildFQName(namespace, "", "active_torrents"),
 			"Number of active torrents.",
 			nil, nil,
 		),
-		pausedTorrents: prometheus.NewDesc(
+		pausedTorrentsDesc: prometheus.NewDesc(
 			prometheus.BuildFQName(namespace, "", "paused_torrents"),
 			"Number of paused torrents.",
 			nil, nil,
 		),
 
-		downloadedBytesTotal: prometheus.NewDesc(
+		downloadedBytesTotalDesc: prometheus.NewDesc(
 			prometheus.BuildFQName(namespace, "", "downloaded_bytes_total"),
 			"Total amount of downloaded data.",
 			nil, nil,
 		),
-		uploadedBytesTotal: prometheus.NewDesc(
+		uploadedBytesTotalDesc: prometheus.NewDesc(
 			prometheus.BuildFQName(namespace, "", "uploaded_bytes_total"),
 			"Total amount of uploaded data.",
 			nil, nil,
@@ -69,14 +63,17 @@ func NewTransmissionCollector(client *transmission.Client, logger log.Logger) (*
 
 // Describe implements the prometheus.Collector interface
 func (t *TransmissionCollector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- t.portOpenDesc
 	ch <- t.turtleModeDesc
+	ch <- t.activeTorrentsDesc
+	ch <- t.pausedTorrentsDesc
+
+	ch <- t.downloadedBytesTotalDesc
+	ch <- t.uploadedBytesTotalDesc
 }
 
 // Collect implements the prometheus.Collector interface.
 func (t *TransmissionCollector) Collect(ch chan<- prometheus.Metric) {
 	fns := []func(chan<- prometheus.Metric){
-		t.collectPortOpen,
 		t.collectTurtleMode,
 		t.collectSessionStats,
 	}
@@ -93,20 +90,6 @@ func (t *TransmissionCollector) Collect(ch chan<- prometheus.Metric) {
 	}
 
 	wg.Wait()
-}
-
-func (t *TransmissionCollector) collectPortOpen(ch chan<- prometheus.Metric) {
-	open, err := t.client.IsPortOpen(context.Background())
-	if err != nil {
-		level.Error(t.logger).Log("msg", "failed to get peer port state", "err", err)
-		return
-	}
-
-	val := 0.
-	if open {
-		val = 1.
-	}
-	ch <- prometheus.MustNewConstMetric(t.portOpenDesc, prometheus.GaugeValue, val)
 }
 
 func (t *TransmissionCollector) collectTurtleMode(ch chan<- prometheus.Metric) {
@@ -130,8 +113,8 @@ func (t *TransmissionCollector) collectSessionStats(ch chan<- prometheus.Metric)
 		return
 	}
 
-	ch <- prometheus.MustNewConstMetric(t.activeTorrents, prometheus.GaugeValue, float64(stats.ActiveTorrents))
-	ch <- prometheus.MustNewConstMetric(t.pausedTorrents, prometheus.GaugeValue, float64(stats.PausedTorrents))
-	ch <- prometheus.MustNewConstMetric(t.downloadedBytesTotal, prometheus.GaugeValue, float64(stats.AllSessions.Downloaded))
-	ch <- prometheus.MustNewConstMetric(t.uploadedBytesTotal, prometheus.GaugeValue, float64(stats.AllSessions.Uploaded))
+	ch <- prometheus.MustNewConstMetric(t.activeTorrentsDesc, prometheus.GaugeValue, float64(stats.ActiveTorrents))
+	ch <- prometheus.MustNewConstMetric(t.pausedTorrentsDesc, prometheus.GaugeValue, float64(stats.PausedTorrents))
+	ch <- prometheus.MustNewConstMetric(t.downloadedBytesTotalDesc, prometheus.GaugeValue, float64(stats.AllSessions.Downloaded))
+	ch <- prometheus.MustNewConstMetric(t.uploadedBytesTotalDesc, prometheus.GaugeValue, float64(stats.AllSessions.Uploaded))
 }
